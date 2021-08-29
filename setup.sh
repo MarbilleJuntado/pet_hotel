faa8e34b302be8632cfbffb9dd8be7aeaf49ea81
Bill Juntado <marbille.juntado@hotmail.ph>
2021-08-29 08:22:19 +0800

    Update yaml

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e # exit on error
-# put ids on .env which docker-compose reads
-printf "USER_ID=$(id -u ${USER})\nGROUP_ID=$(id -g ${USER})" > .env
-docker-compose build
-docker-compose run api mix do deps.get, deps.compile, ecto.setup
